6caadb3c3df3a7fc22df47b022797de08a37c5e7
4010a10 <4010a10@136.145.57.226>
2017-11-15 12:19:23 -0400

    Changed name of JFrame. Changed size & location(in the screen)frame.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,11 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame Joelys = new JFrame("Hello, world!");
+		JFrame Joelys = new JFrame("Hello, world!"); 
 		Joelys.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		Joelys.setLocation(900, 400);
 		Joelys.setSize(200, 200);
-		Joelys.setVisible(true);
+		Joelys.setVisible(true); 
+		
 	}
 }
